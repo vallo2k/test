4754f563ddfe19c1db663cd2ddf9db7ac25e9522
Jerry Liu <jerry@robustintelligence.com>
2023-02-04 15:57:43 -0800

    cr

--- gpt_index/composability/joint_qa_summary.py
+++ gpt_index/composability/joint_qa_summary.py
@@ -6,6 +6,7 @@ from typing import Sequence
 from gpt_index.composability.base import BaseGraphBuilder, Graph
 from gpt_index.indices.base import DOCUMENTS_INPUT, BaseGPTIndex
 from gpt_index.indices.list.base import GPTListIndex
+from gpt_index.indices.tree.base import GPTTreeIndex
 from gpt_index.indices.vector_store.simple import GPTSimpleVectorIndex
 
 DEFAULT_SUMMARY_TEXT = "Use this index for summarization queries"
@@ -49,8 +50,18 @@ class QASummaryGraphBuilder(BaseGraphBuilder):
         vector_index.set_text(qa_text)
         list_index.set_text(summary_text)
 
+        # # used for top-level indices
+        # top_index = GPTSimpleVectorIndex(
+        #     [vector_index, list_index],
+        #     llm_predictor=self._llm_predictor,
+        #     embed_model=self._embed_model,
+        #     docstore=self._docstore,
+        #     index_registry=self._index_registry,
+        #     prompt_helper=self._prompt_helper,
+        # )
+
         # used for top-level indices
-        top_index = GPTSimpleVectorIndex(
+        top_index = GPTTreeIndex(
             [vector_index, list_index],
             llm_predictor=self._llm_predictor,
             embed_model=self._embed_model,
